dec34f881a28253e09436e9760b2db9ec2b7a5a8
B3dRandoms <bdrandoms0@gmail.com>
2023-12-28 00:32:13 +0200

    Initial commit

---
+++ Lab5-2Dudka/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите строку:");
+string input = Console.ReadLine();
+int count = CountDifferentCharacters(input); 
+Console.WriteLine("Количество разных символов: " + count);
+Console.ReadLine();
+static int CountDifferentCharacters(string input)
+{
+    int count = 0; 
+
+    foreach (char c in input)
+    {
+        if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c) && !char.IsSeparator(c)) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
